d4e5cc67e5026c0613a7395c5895ee2547610353
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-03-10 17:37:41 +0530

    Create 3832 - Find Users with Persistent Behavior Patterns.sql

---
+++ 3832 - Find Users with Persistent Behavior Patterns.sql
@@ -0,0 +1,56 @@
+# Using "Gaps and Island" to find the consecutive logins
+WITH first AS (
+    SELECT 
+        user_id,
+        action_date,
+        MAX(action) AS action
+    FROM activity
+    GROUP BY 
+        user_id,
+        action_date
+    HAVING COUNT(user_id) = 1
+),
+
+sec AS (
+    SELECT 
+        user_id,
+        action_date,
+        action,
+        DATE_ADD(
+            action_date, INTERVAL - ROW_NUMBER() OVER(
+                PARTITION BY user_id, action_date
+                ORDER BY action_date
+            ) DAY
+        ) AS grouped
+    FROM first
+),
+
+streak AS (
+    SELECT 
+        user_id,
+        action,
+        COUNT(user_id) AS streak_length,
+        MIN(action_date) AS start_date,
+        MAX(action_date) AS end_date
+    FROM sec
+    GROUP BY 
+        user_id,
+        action
+    HAVING COUNT(user_id) > 4
+)
+
+SELECT 
+    user_id,
+    action,
+    streak_length,
+    start_date,
+    end_date
+FROM streak
+WHERE (user_id, streak_length) IN (
+    SELECT user_id, MAX(streak_length)
+    FROM streak
+    GROUP BY user_id
+)
+ORDER BY 
+    streak_length DESC,
+    user_id ASC
