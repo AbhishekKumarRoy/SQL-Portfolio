630c19bbb1a18a8f0f56aee769e3efc6a1ac7eca
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-05-03 03:31:45 +0530

    Create 1148 - Article Views I.sql

---
+++ Leetcode/1148 - Article Views I.sql
@@ -0,0 +1,8 @@
+-- Proble number is 1148 - Easy
+-- https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY id ASC
