a53b3a8db4ac6d85dccd115bdb24bb754d7d5217
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-05-03 22:13:30 +0530

    Create 1068 - Product Sales Analysis I.sql

---
+++ Leetcode/1068 - Product Sales Analysis I.sql
@@ -0,0 +1,10 @@
+-- Problem number is 1068 - Easy
+-- https://leetcode.com/problems/product-sales-analysis-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Write your PostgreSQL query statement below
+SELECT 
+    p.product_name,
+    s.year,
+    s.price
+FROM Sales s
+INNER JOIN Product p ON s.product_id = p.product_id
