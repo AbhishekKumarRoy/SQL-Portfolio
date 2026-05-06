004da2279976f7d09cde94ce4d93733d716c0e33
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-05-06 23:44:39 +0530

    Create 1581 - Customer Who Visited but Did Not Make Any Transactions.sql

---
+++ Leetcode/1581 - Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,11 @@
+-- Problem number is 1581 - Easy
+-- https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Write your PostgreSQL query statement below
+SELECT 
+    v.customer_id,
+    COUNT(v.customer_id) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+WHERE t.visit_id IS NULL
+GROUP BY v.customer_id
