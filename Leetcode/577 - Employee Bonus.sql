795b9e6e293fbcaac4b45b8484eb348e74a27219
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-05-10 23:46:50 +0530

    Create 577 - Employee Bonus.sql

---
+++ Leetcode/577 - Employee Bonus.sql
@@ -0,0 +1,10 @@
+-- The problem number is 577 - Easy
+-- https://leetcode.com/problems/employee-bonus/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Write your PostgreSQL query statement below
+SELECT 
+    e.name, 
+    b.bonus
+FROM Employee e
+LEFT JOIN Bonus b ON e.empId = b.empId
+WHERE b.bonus IS NULL OR b.bonus < 1000
