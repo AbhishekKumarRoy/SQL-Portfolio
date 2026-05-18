c53b3460373daebdd9af0d82aeb1ce4e6aac6487
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-05-18 23:21:18 +0530

    Create 620 - Not Boring Movies.sql

---
+++ Leetcode/620 - Not Boring Movies.sql
@@ -0,0 +1,12 @@
+-- Problem number is 620 - Easy
+-- https://leetcode.com/problems/not-boring-movies/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Write your PostgreSQL query statement below
+SELECT 
+    id,
+    movie,
+    description,
+    rating
+FROM Cinema
+WHERE (id % 2 <> 0) AND (description <> 'boring')
+ORDER BY rating DESC
