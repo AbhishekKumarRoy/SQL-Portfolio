2ac76c1dab84113ccf0d7b19242f8c0b00f4b762
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-06-01 23:53:37 +0530

    Create 1070 - Product Sales Analysis III.sql

---
+++ Leetcode/1070 - Product Sales Analysis III.sql
@@ -0,0 +1,17 @@
+-- Problem number is 1070 - Medium
+-- https://leetcode.com/problems/product-sales-analysis-iii/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Write your PostgreSQL query statement below
+SELECT 
+    product_id,
+    year AS first_year,
+    quantity,
+    price
+FROM Sales
+WHERE (product_id, year) IN (
+    SELECT 
+        product_id,
+        MIN(year)
+    FROM Sales
+    GROUP BY product_id
+)
