520a2acee797a5120001951b46f98ff263888ce8
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-06-08 23:14:00 +0530

    Create 610 - Triangle Judgement.sql

---
+++ Leetcode/610 - Triangle Judgement.sql
@@ -0,0 +1,13 @@
+-- Problem number is 610 - Easy
+-- https://leetcode.com/problems/triangle-judgement/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Write your PostgreSQL query statement below
+SELECT 
+    x,
+    y,
+    z,
+    CASE
+        WHEN x + y > z AND y + z > x AND x + z > y
+        THEN 'Yes' ELSE 'No'
+    END AS triangle
+FROM Triangle
