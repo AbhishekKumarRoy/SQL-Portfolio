4ea932be8f9231d00a8e107a839e09670f68cbfb
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-06-10 20:19:07 +0530

    Update Y-on-Y Growth Rate.sql

--- DataLemur/Y-on-Y Growth Rate.sql
+++ DataLemur/Y-on-Y Growth Rate.sql
@@ -1,35 +1,33 @@
 -- Sourced from Wayfair - Hard
 -- https://datalemur.com/questions/yoy-growth-rate
 
-WITH total_spend_by_product_per_year AS (
+WITH total_spend_per_year_by_product AS (
   SELECT 
     product_id,
     DATE_PART('YEAR', transaction_date) AS year,
     SUM(spend) AS total_spend
   FROM user_transactions
-  GROUP BY 
+  GROUP BY product_id, DATE_PART('YEAR', transaction_date)
+),
+with_lag AS (
+  SELECT
+    year,
     product_id,
-    DATE_PART('YEAR', transaction_date)
+    total_spend AS curr_year_spend,
+    LAG(total_spend) OVER (
+      PARTITION BY product_id ORDER BY year
+    ) AS prev_year_spend
+  FROM total_spend_per_year_by_product
 )
-SELECT 
+SELECT
   year,
   product_id,
-  total_spend AS curr_year_spend,
-  LAG(total_spend) OVER(
-    PARTITION BY product_id
-    ORDER BY year ASC
-  ) AS prev_year_spend,
+  curr_year_spend,
+  prev_year_spend,
   ROUND(
-    100.00*(total_spend::DECIMAL - LAG(total_spend) OVER(
-      PARTITION BY product_id
-      ORDER BY year ASC
-    ))/LAG(total_spend) OVER(
-      PARTITION BY product_id
-      ORDER BY year ASC
-    ), 2
-  ) AS yoy_rate
-FROM total_spend_by_product_per_year
+    100.00 * (curr_year_spend::DECIMAL - prev_year_spend) / prev_year_spend,
+  2) AS yoy_rate
+FROM with_lag
 ORDER BY 
-  product_id ASC,
-  year ASC
-; 
+  product_id ASC, 
+  year ASC;
