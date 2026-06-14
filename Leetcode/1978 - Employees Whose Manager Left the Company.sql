0a0bac61067cb5dc6edacaeb3a84c1ec2d11ad70
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-06-14 23:37:07 +0530

    Create 1978 - Employees Whose Manager Left the Company.sql

---
+++ Leetcode/1978 - Employees Whose Manager Left the Company.sql
@@ -0,0 +1,11 @@
+-- Problem number is 1978 - Easy
+-- https://leetcode.com/problems/employees-whose-manager-left-the-company/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Write your PostgreSQL query statement below
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000 AND manager_id NOT IN (
+    SELECT employee_id
+    FROM Employees
+)
+ORDER BY employee_id
