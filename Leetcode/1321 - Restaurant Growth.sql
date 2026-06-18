a21391ec9b02d51c326dab64190d6c7267e51972
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-06-18 23:41:54 +0530

    Update 1321 - Restaurant Growth.sql

--- Leetcode/1321 - Restaurant Growth.sql
+++ Leetcode/1321 - Restaurant Growth.sql
@@ -1,7 +1,8 @@
-# 1321. Restaurant Growth
-# https://leetcode.com/problems/restaurant-growth/description/
+-- Problem number is 1321 - Medium
+-- https://leetcode.com/problems/restaurant-growth/description/
 
-# Write your MySQL query statement below
+--1)
+-- Write your MySQL query statement below
 WITH cte AS (
     SELECT 
         visited_on,
@@ -22,4 +23,38 @@ WHERE visited_on >= DATE_ADD((
     FROM Customer), INTERVAL 6 DAY
 )
 GROUP BY visited_on
+ORDER BY visited_on ASC
+
+
+-- 2)    
+-- Write your PostgreSQL query statement below
+WITH cte AS (
+    SELECT 
+        visited_on,
+        SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+),
+cte2 AS (
+    SELECT 
+        visited_on,
+        SUM(amount) OVER(
+            ORDER BY visited_on ASC
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ) AS amount,
+        ROUND(AVG(amount) OVER(
+            ORDER BY visited_on ASC
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ), 2) AS average_amount
+    FROM cte
+)
+
+SELECT 
+    visited_on,
+    amount,
+    average_amount
+FROM cte2
+WHERE visited_on > (
+        SELECT MIN(visited_on) FROM cte2
+    ) + INTERVAL '5 DAYS'
 ORDER BY visited_on ASC
