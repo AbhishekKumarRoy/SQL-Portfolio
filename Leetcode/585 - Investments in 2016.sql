854bd6ee3c195d7fb2fd0d750abef44db8b8119e
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-06-21 18:14:51 +0530

    Create 585 - Investments in 2016.sql

---
+++ Leetcode/585 - Investments in 2016.sql
@@ -0,0 +1,18 @@
+-- Problem number is 585 - Medium
+-- https://leetcode.com/problems/investments-in-2016/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Write your PostgreSQL query statement below
+SELECT 
+    ROUND(SUM(tiv_2016::DECIMAL), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(tiv_2015) > 1
+) AND (lat+lon) IN (
+    SELECT lat+lon
+    FROM Insurance
+    GROUP BY lat+lon
+    HAVING COUNT(lat+lon) = 1
+)
