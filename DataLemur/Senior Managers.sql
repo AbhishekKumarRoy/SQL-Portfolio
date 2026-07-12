ce977eadc0ae34e721c8234cc86d3e6cae6905b5
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-07-12 22:54:47 +0530

    Create Senior Managers.sql

---
+++ DataLemur/Senior Managers.sql
@@ -0,0 +1,13 @@
+-- Sourced from Google - Hard
+-- https://datalemur.com/questions/senior-managers-reportees
+
+SELECT 
+  managers.manager_name,
+  COUNT(DISTINCT managers.emp_id) AS direct_reportees
+FROM employees
+JOIN employees AS managers
+  ON employees.manager_id = managers.emp_id
+JOIN employees AS senior_managers
+  ON managers.manager_id = senior_managers.emp_id
+GROUP BY managers.manager_name
+ORDER BY direct_reportees DESC;
