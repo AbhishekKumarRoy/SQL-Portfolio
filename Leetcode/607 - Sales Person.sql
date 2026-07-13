ceba1faffa7ee54f04662375328436d76058851d
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-07-13 23:45:38 +0530

    Create 607 - Sales Person.sql

---
+++ Leetcode/607 - Sales Person.sql
@@ -0,0 +1,13 @@
+-- Problem number is 607 - Easy
+-- https://leetcode.com/problems/sales-person/description/
+
+-- Write your PostgreSQL query statement below
+SELECT name
+FROM SalesPerson
+WHERE name NOT IN (
+    SELECT s.name
+    FROM SalesPerson s
+    LEFT JOIN Orders o ON s.sales_id = o.sales_id
+    LEFT JOIN Company c ON o.com_id = c.com_id
+    WHERE c.name ='RED'
+)
