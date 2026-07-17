615cfb71a2fb102f2440bdf6a84f3bc969f2e819
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-07-17 19:19:14 +0530

    Create Final Account Balance.sql

---
+++ DataLemur/Final Account Balance.sql
@@ -0,0 +1,13 @@
+-- Sourced from Paypal - Easy
+-- https://datalemur.com/questions/final-account-balance
+
+SELECT 
+  account_id,
+  SUM(
+    CASE
+      WHEN transaction_type = 'Deposit'
+      THEN amount ELSE -amount
+    END
+  ) AS final_balance
+FROM transactions
+GROUP BY account_id;
