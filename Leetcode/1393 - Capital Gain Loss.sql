9de6831af03ea9ff85105afca67cc2efba771039
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-07-23 00:00:59 +0530

    Update 1393 - Capital Gain Loss.sql

--- Leetcode/1393 - Capital Gain Loss.sql
+++ Leetcode/1393 - Capital Gain Loss.sql
@@ -1,6 +1,7 @@
 -- Problem number is 1393 - Medium
 -- https://leetcode.com/problems/capital-gainloss/description/
 
+-- Solution 1
 -- Write your PostgreSQL query statement below
 SELECT
     stock_name,
@@ -10,5 +11,15 @@ SELECT
             THEN -1 * price ELSE price
         END
     ) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
+
+
+-- Solution 2
+-- Write your PostgreSQL query statement below
+SELECT
+    stock_name,
+    SUM(price) FILTER(WHERE operation = 'Sell') - 
+    SUM(price) FILTER(WHERE operation = 'Buy') AS capital_gain_loss
 FROM Stocks
 GROUP BY stock_name
