7a08e02151d52db5276ca8b5c9f3c192f0f152c0
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-07-24 23:38:35 +0530

    Create 1407 - Top Travellers.sql

---
+++ Leetcode/1407 - Top Travellers.sql
@@ -0,0 +1,17 @@
+-- Problem number is 1407 - Easy
+-- https://leetcode.com/problems/top-travellers/description/
+
+-- Write your PostgreSQL query statement below
+SELECT 
+    u.name,
+    COALESCE(
+        SUM(r.distance), 0
+    ) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r ON u.id = r.user_id
+GROUP BY 
+    u.id, 
+    u.name
+ORDER BY 
+    travelled_distance DESC,
+    name ASC
