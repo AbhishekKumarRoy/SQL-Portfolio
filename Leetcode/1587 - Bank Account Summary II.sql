15d58d131d99cb5a1195c33075af1874b197d06e
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-07-26 19:58:04 +0530

    Create 1587 - Bank Account Summary II.sql

---
+++ Leetcode/1587 - Bank Account Summary II.sql
@@ -0,0 +1,11 @@
+-- Problem number is 1587 - Easy
+-- https://leetcode.com/problems/bank-account-summary-ii/description/
+
+-- Write your PostgreSQL query statement below
+SELECT 
+    u.name,
+    SUM(t.amount) AS balance
+FROM Users u
+INNER JOIN Transactions t ON u.account = t.account
+GROUP BY u.name
+HAVING SUM(t.amount) > 10000
