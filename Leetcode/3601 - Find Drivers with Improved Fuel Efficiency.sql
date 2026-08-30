e181aa9f39491a2136823c771deceb8c304a4c5e
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-08-30 22:50:51 +0530

    Update 3601 - Find Drivers with Improved Fuel Efficiency.sql

--- Leetcode/3601 - Find Drivers with Improved Fuel Efficiency.sql
+++ Leetcode/3601 - Find Drivers with Improved Fuel Efficiency.sql
@@ -1,73 +1,77 @@
-# The problem number is 3601 - Medium
-# https://leetcode.com/problems/find-drivers-with-improved-fuel-efficiency/description/
-# There are two solutions for this problem - one solved with CTE and another with Subquery
+-- Problem number is 3601 - Medium
+-- https://leetcode.com/problems/find-drivers-with-improved-fuel-efficiency/description/
 
-  
-# CTE
-WITH first AS (
+-- Solution 1 (Simple and easy to understand)
+-- Write your PostgreSQL query statement below
+WITH first_half AS (
     SELECT 
+        trip_id,
         driver_id,
-        SUM(distance_km/fuel_consumed)/COUNT(driver_id) AS first_half_avg
+        trip_date,
+        distance_km,
+        fuel_consumed,
+        distance_km/fuel_consumed AS fuel_efficiency
     FROM trips
-    WHERE trip_date BETWEEN '2023-01-01' AND '2023-06-30'
-    GROUP BY driver_id
-),
-second AS (
+    WHERE DATE_PART('MONTH', trip_date) BETWEEN '1' AND '6'
+), 
+second_half AS (
     SELECT 
+        trip_id,
         driver_id,
-        SUM(distance_km/fuel_consumed)/COUNT(driver_id) AS second_half_avg
+        trip_date,
+        distance_km,
+        fuel_consumed,
+        distance_km/fuel_consumed AS fuel_efficiency
     FROM trips
-    WHERE trip_date BETWEEN '2023-07-01' AND '2023-12-31' 
-    GROUP BY driver_id
+    WHERE DATE_PART('MONTH', trip_date) BETWEEN '7' AND '12'
 )
+
 SELECT 
     f.driver_id,
     d.driver_name,
-    ROUND(f.first_half_avg, 2) AS first_half_avg,
-    ROUND(s.second_half_avg, 2) AS second_half_avg,
-    ROUND(s.second_half_avg - f.first_half_avg, 2) AS efficiency_improvement
-FROM first f
-INNER JOIN 
-    drivers d ON f.driver_id = d.driver_id
-INNER JOIN 
-    second s ON f.driver_id = s.driver_id
-WHERE 
-    s.second_half_avg > f.first_half_avg
+    ROUND(AVG(f.fuel_efficiency), 2) AS first_half_avg,
+    ROUND(AVG(s.fuel_efficiency), 2) AS second_half_avg,
+    ROUND(
+        AVG(s.fuel_efficiency) - AVG(f.fuel_efficiency), 2
+    ) AS efficiency_improvement
+FROM first_half f
+INNER JOIN second_half s ON f.driver_id = s.driver_id
+INNER JOIN drivers d ON f.driver_id = d.driver_id
+GROUP BY 
+    f.driver_id,
+    d.driver_name
+HAVING ROUND(
+        AVG(s.fuel_efficiency) - AVG(f.fuel_efficiency), 2
+    ) > 0
 ORDER BY 
     efficiency_improvement DESC,
     driver_name ASC
 
-#-------------------------------------------------------------------------------------------
 
-# Subquery
+-- Solution 2 (No CTE, Only Joins)
+-- Write your PostgreSQL query statement below
 SELECT 
     f.driver_id,
     d.driver_name,
-    ROUND(f.first_half_avg, 2) AS first_half_avg,
-    ROUND(s.second_half_avg, 2) AS second_half_avg,
-    ROUND(s.second_half_avg - f.first_half_avg, 2) AS efficiency_improvement
-FROM 
-    (
-        SELECT 
-            driver_id,
-            SUM(distance_km / fuel_consumed) / COUNT(driver_id) AS first_half_avg
-        FROM trips
-        WHERE trip_date BETWEEN '2023-01-01' AND '2023-06-30'
-        GROUP BY driver_id
-    ) f
-JOIN 
-    (
-        SELECT 
-            driver_id,
-            SUM(distance_km / fuel_consumed) / COUNT(driver_id) AS second_half_avg
-        FROM trips
-        WHERE trip_date BETWEEN '2023-07-01' AND '2023-12-31'
-        GROUP BY driver_id
-    ) s ON f.driver_id = s.driver_id
-JOIN 
-    drivers d ON f.driver_id = d.driver_id
-WHERE 
-    s.second_half_avg > f.first_half_avg
+    ROUND(AVG(f.distance_km/f.fuel_consumed), 2) AS first_half_avg,
+    ROUND(AVG(s.distance_km/s.fuel_consumed), 2) AS second_half_avg,
+    ROUND(
+        AVG(s.distance_km/s.fuel_consumed) - 
+        AVG(f.distance_km/f.fuel_consumed), 2
+    ) AS efficiency_improvement
+FROM trips f
+INNER JOIN trips s ON f.driver_id = s.driver_id 
+INNER JOIN drivers d ON f.driver_id = d.driver_id
+    AND DATE_PART('MONTH', f.trip_date) BETWEEN 1 AND 6
+    AND DATE_PART('MONTH', s.trip_date) BETWEEN 7 AND 12
+GROUP BY 
+    f.driver_id,
+    d.driver_name
+HAVING 
+    ROUND(
+        AVG(s.distance_km/s.fuel_consumed) - 
+        AVG(f.distance_km/f.fuel_consumed), 2
+    ) > 0
 ORDER BY 
     efficiency_improvement DESC,
-    driver_name ASC;
+    driver_name ASC
