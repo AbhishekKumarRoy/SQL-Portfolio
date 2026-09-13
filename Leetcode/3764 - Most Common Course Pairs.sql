3597f8edb463d40ca2eaf9ed53a25ba5a06470d3
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-09-13 23:35:36 +0530

    Update 3764 - Most Common Course Pairs.sql

--- Leetcode/3764 - Most Common Course Pairs.sql
+++ Leetcode/3764 - Most Common Course Pairs.sql
@@ -1,2 +1,39 @@
 -- Problem number is 3764 - Hard
 -- https://leetcode.com/problems/most-common-course-pairs/description/
+
+-- Write your PostgreSQL query statement below
+WITH top_performer AS (
+    SELECT user_id
+    FROM course_completions
+    GROUP BY user_id
+    HAVING 
+        COUNT(course_id) > 4
+        AND AVG(course_rating) >= 4
+),
+sequence AS (
+    SELECT
+        c.user_id,
+        c.course_id,
+        c.course_name AS first_course,
+        LEAD(c.course_name) OVER(
+            PARTITION BY c.user_id 
+            ORDER BY c.completion_date ASC
+        ) AS second_course
+    FROM course_completions c
+    INNER JOIN top_performer t ON c.user_id = t.user_id
+)
+
+SELECT
+    first_course,
+    second_course,
+    COUNT(*) AS transition_count
+FROM sequence
+WHERE second_course IS NOT NULL
+GROUP BY 
+    first_course,
+    second_course
+ORDER BY 
+    transition_count DESC,
+-- Using LOWER to sort correctly (due to the comparison mechanism based on the binary value (ASCII Code) of each character)
+    LOWER(first_course) ASC,
+    LOWER(second_course) ASC
