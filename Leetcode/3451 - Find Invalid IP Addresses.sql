c166825c06c339ad457328e3e077075eff1e6abc
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-09-19 23:20:03 +0530

    Update 3451 - Find Invalid IP Addresses.sql

--- Leetcode/3451 - Find Invalid IP Addresses.sql
+++ Leetcode/3451 - Find Invalid IP Addresses.sql
@@ -1,2 +1,13 @@
 -- Problem number is 3451 - Hard
 -- https://leetcode.com/problems/find-invalid-ip-addresses/description/
+
+-- Write your PostgreSQL query statement below
+SELECT
+    ip,
+    COUNT(*) AS invalid_count
+FROM logs
+WHERE ip !~ '^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$'
+GROUP BY ip
+ORDER BY 
+    invalid_count DESC, 
+    ip DESC;
